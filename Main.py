7fec2aefd450800c20aa3fa92cd912e96129f790
FloriVonDerGPC <florian_ries@icloud.com>
2018-06-07 18:18:45 +0200

    time added

--- Main.py
+++ Main.py
@@ -1,5 +1,14 @@
 # MAIN
 
-# TODO: Test
+import datetime
+from ctypes import c_bool
+
 print("Log: Start Python")
+
+def date():
+    nowDate = datetime.datetime.now()
+    return [nowDate.year, nowDate.month, nowDate.day, nowDate.hour, nowDate.minute, nowDate.second]
+
+currentTime = date()
+print(currentTime)
 
