a58fe33b357734f24d887e0571c2574627faf2b2
Claudio H <claudio.homeyer@gmail.com>
2018-06-07 23:14:11 +0200

    JSONHandler comments

--- JSONHandler.py
+++ JSONHandler.py
@@ -1,6 +1,11 @@
+#Import json library for easy access
 import json
 
+#Function: read json file and return 'par_country'
 def ReadJSON (var_path):
 
+    #load document
     var_response = json.load(open(var_path))
+
+    #find and return 'par_country'
     return var_response['par_country']
