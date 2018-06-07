b63c2f116793371ecc9edc13e41d182de7abd743
FloriVonDerGPC <florian_ries@icloud.com>
2018-06-07 18:37:22 +0200

    time added 3

--- Main.py
+++ Main.py
@@ -3,13 +3,17 @@
 import datetime
 from ctypes import c_bool
 
-print("Log: Start Python")
+######## DATABASE #######
+countryPlay = {'Deutschland': '21.12.2018', 'Spanien': '01.06.2019', 'Sizilien': '01.01.2018'}
+#########################
 
-def date():
+
+def getDate():
     nowDate = datetime.datetime.now()
     return [nowDate.year, nowDate.month, nowDate.day, nowDate.hour, nowDate.minute, nowDate.second]
 
-currentTime = date()
-print(currentTime)
+def findCountry(search, countries):
+    return countries[search]
 
-print("Hallo Flo")
+playDate = findCountry('Deutschland', countryPlay)
+print("Game is on " + str(playDate))
