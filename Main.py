61091d41044ed412ecdc20bc19ac2d9bb2da083b
FloriVonDerGPC <florian_ries@icloud.com>
2018-06-07 18:37:42 +0200

    time added 3

--- Main.py
+++ Main.py
@@ -7,7 +7,6 @@ from ctypes import c_bool
 countryPlay = {'Deutschland': '21.12.2018', 'Spanien': '01.06.2019', 'Sizilien': '01.01.2018'}
 #########################
 
-
 def getDate():
     nowDate = datetime.datetime.now()
     return [nowDate.year, nowDate.month, nowDate.day, nowDate.hour, nowDate.minute, nowDate.second]
