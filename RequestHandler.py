5212a77a8f3b766975891d20777bea48ea1bca2a
Claudio Homeyer <xershoxx@users.noreply.github.com>
2018-06-08 11:34:08 +0200

    Created new class RequestHandler

---
+++ RequestHandler.py
@@ -0,0 +1,25 @@
+#Import json library for easy access
+import json
+
+#Class: handler to process requests
+class RequestHandler:
+    
+    #Init
+    def __init__(self):       
+        #variable definition
+        self.var_request = []    
+    
+    #Function: Load Request
+    def LoadRequest(self, var_file):     
+        #open file and load into "var_request"
+        self.var_request = json.load(open(var_file))
+    
+    #Function: return "par_country"
+    def GetCountry (self):
+        #find and return "par_country"
+        return self.var_request["queryResult"]["parameters"]["par_country"]
+    
+    #Function: return "action"
+    def GetAction (self):
+        #find and return "action"
+        return self.var_request["queryResult"]["action"]
