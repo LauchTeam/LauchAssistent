b9d148a79b75428dc17ae4e5299c956126fa4686
Claudio H <claudio.homeyer@gmail.com>
2018-06-08 17:39:38 +0200

    Added Comments

--- HandlerDB.py
+++ HandlerDB.py
@@ -1,27 +1,36 @@
-# import mysql for easy db communication
+# Import mysql for easy db communication
 import mysql.connector
 
 class HandlerDB:
     
-    #Init
+    # Init
     def __init__(self):       
         #variable definition
         self.DB = ""
     
+    # Function: Connect to Database
     def ConntectTo(self, var_user, var_pass, var_host, var_db):
         try:
+            # Establish Connection
             self.DB = mysql.connector.connect(user=var_user, password=var_pass, host=var_host, database=var_db)
-            return "Successful connected"
+            # return success
+            return True
         except:
-            return "No Connection"
-        
+            # Error message
+            return False
+    
+    # Function: Execute SQL Statement and return result
     def Execute(self, sql_statement):
+        # Execute SQL Statement
         self.Exec = self.DB.cursor()
         self.Exec.execute(sql_statement)
 
+        # Save result
         result = self.Exec.fetchall()
 
+        # return result
         return result
 
+    # Funtion: Close Connection
     def CloseConnection(self):
         self.DB.close()
