3143db79eac378ee1eddcdbb377131f8785fce4d
Claudio Homeyer <xershoxx@users.noreply.github.com>
2018-06-08 10:44:51 +0200

    redefined parsing
    redefined parsing to be compatible with full request

--- JSONHandler.py
+++ JSONHandler.py
@@ -8,4 +8,4 @@ def ReadJSON (var_path):
     var_response = json.load(open(var_path))
 
     #find and return 'par_country'
-    return var_response['par_country']
+    return var_response["queryResult"]["parameters"]["par_country"]
