18d204d8969aecac6cf5b71b4ec1ef1f7c069e54
claudio.homeyer@gmail.com <Wonderboy93>
2018-09-08 00:02:51 +0200

    Setup of DB

--- lauchassistent.py
+++ lauchassistent.py
@@ -1,12 +1,15 @@
 from flask import Flask, request, Response, json
+import handlerdb
 app = Flask(__name__)
 
 
 @app.route('/lauchassistent', methods=['GET', 'POST'])
 def hello_world():
+    db = handlerdb.HandlerDB()
+
     data = request.get_json()
     print(data["queryResult"]["parameters"]["par_country"])
-    message = "Schland"
+    message = db.connectTo()
 
     answer = {"fulfillmentText": "hello world", "fulfillmentMessages": [{"text": {"text": [message]}}]}
     js = json.dumps(answer)
@@ -14,5 +17,3 @@ def hello_world():
     resp = Response(js, status=200, mimetype='application/json')
     resp.headers['test'] = 'Testheader'
     return resp
-
-# dsds
