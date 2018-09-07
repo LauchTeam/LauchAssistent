2394f7c4cade93455e250f7679692eaff409925d
claudio.homeyer@gmail.com <Wonderboy93>
2018-09-08 00:17:47 +0200

    Setup of DB

--- lauchassistent.py
+++ lauchassistent.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 @app.route('/lauchassistent', methods=['GET', 'POST'])
 def hello_world():
     db = handlerdb.HandlerDB()
-
     data = request.get_json()
-    print(data["queryResult"]["parameters"]["par_country"])
-    message = db.connectTo()
+
+    kpi_name = data["queryResult"]["parameters"]["par_country"]
+    kpi_value = db.Execute("SELECT kpi_value FROM finance.table_kpi WHERE kpi_name = '" + data["queryResult"]["parameters"]["var_kpi"] + "';")
+
+    # print(data["queryResult"]["parameters"]["par_country"])
+    message = "Die KPI " + kpi_name + "hat folgenden Wert: " + kpi_value
 
     answer = {"fulfillmentText": "hello world", "fulfillmentMessages": [{"text": {"text": [message]}}]}
     js = json.dumps(answer)
